eae92b04f55279d06b50df3584ab9044019ff0ad
techie0569 <07219991p@gmail.com>
2025-02-16 20:37:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// You can add interactive features here, like a smooth scroll or animations
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
